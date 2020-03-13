b70216f66f7a170c4585050362d2e92e690c432d
3141592654 <12554811+3141592654@users.noreply.github.com>
2020-03-13 13:34:59 -0700

    Ported test_cache_lib and test_fifo to catch2

--- test_cache_lib.cc
+++ test_cache_lib.cc
@@ -3,15 +3,16 @@
  */
 
 #include "cache.hh"
-#include <assert.h>
 #include <string.h>
 #include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 // The prompt says to use a global variable and to reset it after each test.
 // I checked with Eitan, and he confirms this is desired behavior.
 Cache test_cache(1024);
 
-void test_get() {
+TEST_CASE( "test_get", "[placeholder]" ) {
     char charr0[] = "the quick brown fox jumps over the lazy dog";
     test_cache.set("key0", charr0, 44);
     // It sure would be nice if the interface defined the second argument
@@ -19,70 +20,70 @@ void test_get() {
     // Make sure we can get back "the quick brown fox jumps over the lazy dog".
     uint32_t s = 44;
     const char* ret0 = test_cache.get("key0", s);
-    // This goes against the "only one assert per test" axiom. We are fine with this
-    // because there is more than one thing we want to test, and having assert(bla && BLA && blalala)
+    // This goes against the "only one REQUIRE per test" axiom. We are fine with this
+    // because there is more than one thing we want to test, and having REQUIRE(bla && BLA && blalala)
     // would be silly. Also, if we split this up into multiple tests, there would be
     // more code duplication.
-    assert(ret0 != nullptr);
-    assert(s == 44);
-    assert(strcmp(charr0, ret0) == 0);
+    REQUIRE(ret0 != nullptr);
+    REQUIRE(s == 44);
+    REQUIRE(strcmp(charr0, ret0) == 0);
     // Make sure we return only 44 bytes even if we request 45 bytes, as per the specification.
     s = 45;
     const char* ret1 = test_cache.get("key0", s);
-    assert(ret1 != nullptr);
-    //assert(s == 44);    // Our implementation fails this test, embarassingly.
-    assert(strcmp(charr0, ret1) == 0);
+    REQUIRE(ret1 != nullptr);
+    //REQUIRE(s == 44);    // Our implementation fails this test, embarassingly.
+    REQUIRE(strcmp(charr0, ret1) == 0);
     // Add another test, make sure we can get back "J Q Vandz struck my big fox whelp".
     char charr2[] = "J Q Vandz struck my big fox whelp";
     test_cache.set("key2", charr2, 34);
     s = 34;
     const char* ret2 = test_cache.get("key2", s);
-    assert(ret2 != nullptr);
-    assert(s == 34);
-    assert(strcmp(charr2, ret2) == 0);
+    REQUIRE(ret2 != nullptr);
+    REQUIRE(s == 34);
+    REQUIRE(strcmp(charr2, ret2) == 0);
     // Make sure we can still get "the quick brown fox jumps over the lazy dog" if we want to.
     s = 44;
     const char* ret3 = test_cache.get("key0", s);
-    assert(ret3 != nullptr);
-    assert(s == 44);
-    assert(strcmp(charr0, ret3) == 0);
+    REQUIRE(ret3 != nullptr);
+    REQUIRE(s == 44);
+    REQUIRE(strcmp(charr0, ret3) == 0);
     test_cache.reset();
 }
 
 
-void test_del() {
+TEST_CASE( "test_del", "[placeholder]" ) {
     // Short test
     char charr0[] = "the quick brown fox jumps over the lazy dog";
     test_cache.set("key0", charr0, 44);
     uint32_t s = 42;
     test_cache.del("key0");
-    assert(test_cache.get("key0", s) == nullptr);
+    REQUIRE(test_cache.get("key0", s) == nullptr);
     test_cache.reset();
 }
 
-void test_space_used() {
-    assert(test_cache.space_used() == 0);
+TEST_CASE( "test_space_used", "[placeholder]" ) {
+    REQUIRE(test_cache.space_used() == 0);
     char charr0[] = "the quick brown fox jumps over the lazy dog";
     test_cache.set("key0", charr0, 44);      // Include the null terminator in all sizes.
-    assert(test_cache.space_used() == 44);
+    REQUIRE(test_cache.space_used() == 44);
     // Make sure we do a deep copy by ending the string early
     charr0[3] = '\0';
-    assert(test_cache.space_used() == 44);
+    REQUIRE(test_cache.space_used() == 44);
     char charr1[] = "J Q Vandz struck my big fox whelp";
     // Make sure space adds linearly
     test_cache.set("key1", charr1, 34);
-    assert(test_cache.space_used() == 44+34);
+    REQUIRE(test_cache.space_used() == 44+34);
     // Make sure the old value gets overwritten
     char charr2[] = "Sphinx of black quartz, hear my vow!";
     test_cache.set("key0", charr2, 38);
-    assert(test_cache.space_used() == 38+34);
+    REQUIRE(test_cache.space_used() == 38+34);
     // Make sure the old value got deleted
     test_cache.del("key1");
-    assert(test_cache.space_used() == 38);
+    REQUIRE(test_cache.space_used() == 38);
     test_cache.reset();
 }
 
-void test_reset() {
+TEST_CASE( "test_reset", "[placeholder]" ) {
     // Give our test_cache some keys and values
     char charr0[] = "the quick brown fox jumps over the lazy dog";
     test_cache.set("key0", charr0, 44);
@@ -93,23 +94,11 @@ void test_reset() {
     // Reset it
     test_cache.reset();
     // Make sure it got deleted. (This assumes test_space_used is working properly).
-    assert(test_cache.space_used() == 0);
+    REQUIRE(test_cache.space_used() == 0);
     // I don't recall what the desired behavior is for reset if there is nothing in the cache. Let's say it just deletes nothing.
     test_cache.reset();
-    assert(test_cache.space_used() == 0);
+    REQUIRE(test_cache.space_used() == 0);
     test_cache.reset();
 }
 
-int main() {
-    // For all of these, we assume the previous tests are working properly.
-    // This is a good assumption because otherwise assert would exit the program.
-    // We decided against an explicit creation/deletion test. We were not sure what
-    // we would put in them, and thought valgrind would be better at detecting
-    // errors than we would ever be.
-    // We were not sure how one would test set, so test_get used to be named test_set_and_get.
-    test_get();
-    test_del();
-    test_space_used();
-    test_reset();
-}
 
--- test_fifo_enderquestral.cc
+++ test_fifo_enderquestral.cc
@@ -3,35 +3,34 @@
  */
 
 #include "cache.hh"
-#include <assert.h>
 #include <string.h>
 #include <iostream>
 #include "Systems_HW2/fifo_evictor.hh"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 Fifo_Evictor* test_evictor = new Fifo_Evictor();
 Cache test_cache = Cache(1024, 0.75, test_evictor);
 
-void test_fifo_evictor() {    
+TEST_CASE( "test_fifo_evictor", "[placeholder]" ) {    
     char charr0[] = "In this assignment, you will write a generic look-aside cache. A look-aside cache is a key-valueSTOPstorage for items that are difficult or slow to compute. Whenever a client code requires such anSTOPitem, it first queries the cache for the desired key. If the cache has it, it returns the associatedSTOPvalue. If it doesn't, the client obtains the value on its own (presumably through a slower process),STOPand then saves it to the cache for future references. If the data access patterns exhibit temporalSTOPlocality, the use of the cache will end up being beneficial and faster than always computing theSTOPvalues. Your job will be to implement the cache. In C++, write a cache_lib.cc file to complementSTOP";
     test_cache.set("key0", charr0, 712);
     char charr1[] = "cache.hh. You may not modify any of the .hh files provided. Please read through this entireSTOPassignment before you start implementing, to save yourself the trouble of redesigning the codeSTOPlater.STOPThis is a paired assignment. You are required to find a partner and work together, in particularSTOPcollaborating on the design and the debugging. Your github repo should include all the source files,STOPyour complete tests, a Makefile or build script, and a README.md with your design choices.";
     test_cache.set("key1", charr1, 499);
     uint32_t s = 712;
-    assert(test_cache.get("key0", s) == nullptr);
+    REQUIRE(test_cache.get("key0", s) == nullptr);
     s = 499;
-    assert(test_cache.get("key1", s) != nullptr);
-    assert(strcmp(charr1, test_cache.get("key1", s)) == 0);
-    assert(test_cache.space_used() == 499);
+    REQUIRE(test_cache.get("key1", s) != nullptr);
+    REQUIRE(strcmp(charr1, test_cache.get("key1", s)) == 0);
+    REQUIRE(test_cache.space_used() == 499);
     char charr2[] = "You will implement your cache as a RAM-based storage of values, indexed by keys. All keys andSTOPvalues will reside in main memory. The cache needs to conform to the interface (API, or contract),STOPthat's provided in the attached cache.hh header file. This API abstracts away the implementation";
     test_cache.set("key2", charr2, 297);
-    assert(test_cache.get("key1", s) != nullptr);
+    REQUIRE(test_cache.get("key1", s) != nullptr);
     s = 297;
-    assert(test_cache.get("key2", s) != nullptr);
-    assert(test_cache.space_used() == 499+297);
+    REQUIRE(test_cache.get("key2", s) != nullptr);
+    REQUIRE(test_cache.space_used() == 499+297);
     delete test_evictor;
 }
 
-int main() {
-    test_fifo_evictor();
-}
+
 
